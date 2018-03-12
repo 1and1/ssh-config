7179a30a6b43e48a28ce404acebd66fcc8cff004
Stephan Fuhrmann <stephan.fuhrmann@1und1.de>
2018-03-12 15:13:21 +0100

    Added license header

--- src/main/java/com/oneandone/sshconfig/bind/package-info.java
+++ src/main/java/com/oneandone/sshconfig/bind/package-info.java
@@ -13,26 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.oneandone.sshconfig;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 
 /**
- *
- * @author Stephan Fuhrmann
+ * Mappings for binding Java-types to JSON.
  */
-public class ParamsTest {
-    @Test
-    public void testParseWithoutUser() {
-        Params params = Params.parse(new String [] {});
-        assertNotNull(params.getUser());
-    }
-    
-    @Test
-    public void testParseWithUser() {
-        Params params = Params.parse(new String [] {"-user", "foomaster"});
-        assertEquals("foomaster", params.getUser());
-    }
-}
+package com.oneandone.sshconfig.bind;
